352e3f8107886391ed6877b19507e92d3a1d01d1
teacup-on-rockingchair <strandjata@gmail.com>
2013-01-28 16:52:41 +0200

    links changes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -41,4 +41,19 @@ describe "StaticPages" do
                               :text => "#{base_title} | Contacts") }
   end
 
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Us')
+    click_link "Help"
+    page.should have_selector 'title', text: full_title('Help')
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Contacts')
+    click_link "Home"
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: full_title('Sign Up')
+    click_link "sample app"
+    page.should # fill in
+  end
+
 end
